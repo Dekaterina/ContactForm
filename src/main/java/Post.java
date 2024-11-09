adde3d2bd11d0978ca6d110fce4dd8d2f6a51c13
Ekaterina Romanova <Dekaterina83@mail.ru>
2024-11-09 16:24:47 +0300

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,23 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormData birthday;
+    public String day;
+    public String month;
+    public String year;
+
+    public static class FormData {
+        public String day;
+        public String month;
+        public String year;
+
+
+    }
+}
+
+
+
