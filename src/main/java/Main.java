db757597ff6d82f9608043674c610f50898220ce
Ekaterina Romanova <Dekaterina83@mail.ru>
2024-11-11 21:44:47 +0300

    extra lines removed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,13 @@ public class Main {
         post.name = "Иван";
         post.passport = "4444 № 44444444";
         post.patronymic = "Иванович";
-        post.phone = "+7(999)-999-99-99";
+        post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
-        post.subscription = "true";
-        post.birthday = new Post.FormData();
-        post.day = "13";
-        post.month = "6";
-        post.year = "1999";
+        post.subscription = true;
 
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
     }
-
 }
--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,23 +1,13 @@
+
 public class Post {
     public String name;
     public String passport;
     public String patronymic;
     public String phone;
     public String surname;
-    public String subscription;
-    public FormData birthday;
-    public String day;
-    public String month;
-    public String year;
+    public boolean subscription;
+    public FormDate birthday;
 
-    public static class FormData {
-        public String day;
-        public String month;
-        public String year;
-
-
-    }
 }
-
 
 
